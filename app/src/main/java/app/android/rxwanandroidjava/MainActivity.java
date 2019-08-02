3f12ce78791785f476ee9350d4b0a7b4e3fb59a0
xiaowh <932599583@qq.com>
2019-08-02 15:56:31 +0800

    init

---
+++ app/src/main/java/app/android/rxwanandroidjava/MainActivity.java
@@ -0,0 +1,52 @@
+package app.android.rxwanandroidjava;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_system:
+                    mTextMessage.setText(R.string.title_system);
+                    return true;
+                case R.id.navigation_gzh:
+                    mTextMessage.setText(R.string.title_gzh);
+                    return true;
+                case R.id.navigation_project:
+                    mTextMessage.setText(R.string.title_project);
+                    return true;
+                case R.id.navigation_mine:
+                    mTextMessage.setText(R.string.title_mine);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
